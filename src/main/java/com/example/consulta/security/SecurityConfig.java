6b4a06ddc7f340e379104bb8e90decb1159fa354
AndriuuU <66785221+AndriuuU@users.noreply.github.com>
2023-06-06 23:32:17 +0200

    aplicado Web

--- src/main/java/com/example/consulta/security/SecurityConfig.java
+++ src/main/java/com/example/consulta/security/SecurityConfig.java
@@ -17,31 +17,25 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable().addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class).
-		authorizeHttpRequests((requests) -> requests.
-			requestMatchers("/**").permitAll());
-//			);
-//			requestMatchers("/", "/imgs/","/photos/","/auth/**","/webjars/**","/css/**","/noticias/**","/files/**").permitAll().
-//			requestMatchers("/api/admin/**").hasRole("ADMIN").
-//			requestMatchers("/api/user/**").hasRole("USER").
-//			requestMatchers("/api/all/**").hasAnyRole("USER","ADMIN").	
-//			
-//			//WEB
-//			
-//		
-//		http.authorizeRequests((requests)->requests
-//			.requestMatchers("/cliente/admin/**","/emple/admin/**","/cita/admin/**where").hasRole("ADMIN")
-//			.requestMatchers("/css/**").permitAll()
-//			.requestMatchers("/**").permitAll())
-////			.anyRequest().authenticated())
-//			.formLogin((form)->form
-//			       .loginPage("/auth/login")
-//			       .defaultSuccessUrl("/home",true)
-//			       .permitAll())
-////			        
-//			.logout((logout)->logout.permitAll()
-//					.logoutUrl("/auth/logout")
-//					.logoutSuccessUrl("/auth/login?logout"));
+//
+//		http.csrf().disable().addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class).
+//		authorizeHttpRequests((requests) -> requests.
+//			requestMatchers("/**").permitAll());
+			//WEB
+		
+		http.authorizeRequests((requests)->requests
+			.requestMatchers("/cliente/admin/**","/cita/admin/**where").hasRole("ADMIN")
+			.requestMatchers("/cita/cliente/**").hasRole("USER")
+			.requestMatchers("/css/**").permitAll()
+			.requestMatchers("/**").permitAll().anyRequest().authenticated())
+			.formLogin((form)->form
+			       .loginPage("/auth/login")
+			       .defaultSuccessUrl("/home",true)
+			       .permitAll())
+//			        
+			.logout((logout)->logout.permitAll()
+					.logoutUrl("/auth/logout")
+					.logoutSuccessUrl("/auth/login?logout"));
 
 		return http.build();
 	}
